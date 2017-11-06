996082093a52e1c5a3dec35f7b91d2d8a70ef1d4
Land <ludidilu@163.com>
2017-11-07 00:17:44 +0800

    a

---
+++ csv/sds/DishSDS.cs
@@ -0,0 +1,69 @@
+﻿using Cook_lib;
+
+public class DishSDS : CsvBase, IDishSDS
+{
+    public float prepareTime;
+    public float prepareDecreaseValue;
+    public float cookTime;
+    public float optimizedTime;
+    public float optimizeDecreaseValue;
+    public float exceedTime;
+    public bool isUniversal;
+    public int maxNum;
+    public int money;
+    public int moneyOptimized;
+
+    public int GetID()
+    {
+        return ID;
+    }
+
+    public float GetPrepareTime()
+    {
+        return prepareTime;
+    }
+
+    public float GetPrepareDecreaseValue()
+    {
+        return prepareDecreaseValue;
+    }
+
+    public float GetCookTime()
+    {
+        return cookTime;
+    }
+
+    public float GetOptimizeTime()
+    {
+        return optimizedTime;
+    }
+
+    public float GetOptimizeDecreaseValue()
+    {
+        return optimizeDecreaseValue;
+    }
+
+    public float GetExceedTime()
+    {
+        return exceedTime;
+    }
+
+    public bool GetIsUniversal()
+    {
+        return isUniversal;
+    }
+    public int GetMaxNum()
+    {
+        return maxNum;
+    }
+
+    public int GetMoney()
+    {
+        return money;
+    }
+
+    public int GetMoneyOptimized()
+    {
+        return moneyOptimized;
+    }
+}
